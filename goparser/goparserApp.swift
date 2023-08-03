76472f5cb4204c35e87fabae02502b0eaf16e452
spectatorMrZ <zhuchunan001@sina.com>
2023-08-03 16:01:44 +0800

    Initial Commit

---
+++ goparser/goparserApp.swift
@@ -0,0 +1,17 @@
+//
+//  goparserApp.swift
+//  goparser
+//
+//  Created by spec on 2023/8/3.
+//
+
+import SwiftUI
+
+@main
+struct goparserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
